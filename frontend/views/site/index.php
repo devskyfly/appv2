bbccec369f83393a5a1bd91580d694d70d45d51a
Filippov Ivan <devskyfly@gmail.com>
2020-04-12 21:52:16 +0300

    Add alert boxes.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,19 +9,19 @@ use yii\helpers\Url;
 
 <div class="raw">
     <div class="col-xs-12  col-md-6">
-        <div class="well">
+        <div class="well" style ="background-image: url('/img/alert.jpg?v1'); background-size: cover">
             <div style="background-color: white;padding: 20px;">
                 <a href = "<?=Url::toRoute(["/article/sobraniye"])?>" style="color: gray; text-decoration: underline;">
-                    <h3 style="text-align: center;">Общее собрание 2020-03-14</h3>
+                    <h3 style="text-align: center; color: #ff7200; text-decoration-color: #ff7200;">Собрание после снятия самоизоляции</h3>
                 </a>
             </div>
         </div>
     </div>
     <div class="col-xs-12  col-md-6">
-        <div class="well">
+        <div class="well" style ="background-image: url('/img/alert.jpg?v1'); background-size: cover">
             <div style="background-color: white;padding: 20px;">
                 <a href = "<?=Url::toRoute(["/article/snos"])?>" style="color: gray; text-decoration: underline;">
-                    <h3 style="text-align: center;">Снос</h3>
+                    <h3 style="text-align: center; color: #ff7200; text-decoration-color: #ff7200;">Снос</h3>
                 </a>
             </div>
         </div>
